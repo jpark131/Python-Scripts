048f55eccad48596270bc0dd4837c7a6007361c3
Jake Park <jake.park2398@gmail.com>
2023-10-01 17:42:40 -0700

    Initial Commit

---
+++ packages/ping.py
@@ -0,0 +1,25 @@
+"""Quick library for pinging a network device"""
+import os
+import shlex
+import subprocess as sp
+
+def ping(hostname, num_pings=4):
+    """
+    Purpose: ping a network host. Works on windows and posix systems
+    Parameters:
+        -hostname: hostname or ip of network host to be pinged
+        -num_pings: number of times to attempt ping
+    Return: True if success, False otherwise
+    """
+    if os.name == "nt":
+        pingstring = f"ping -n {num_pings} {hostname}"
+    elif os.name == "posix":
+        pingstring = f"ping -c {num_pings} {hostname}"
+    else:
+        print(f"OS {os.name} not handled")
+        return False
+    child = sp.Popen(shlex.split(pingstring), 
+                     universal_newlines=True, 
+                     stdout=sp.PIPE, stderr=sp.PIPE)
+    child_stdout = child.communicate()[0]
+    return "bytes" in child_stdout
---
+++ ssh_executor.py
@@ -0,0 +1,30 @@
+"""Execute a command on multiple hosts"""
+
+import os
+import sys
+import shlex
+import subprocess as sp
+from packages.ping import ping
+sys.path.append('C:/Users/jakep/Documents/code/packages')
+
+def open_subprocess(command):
+    """Use subprocess.Popen to open a subprocess and return the child process object"""
+    child = sp.Popen(shlex.split(command),
+                     universal_newlines=True,
+                     stdout=sp.PIPE, stderr=sp.STDOUT)
+    return child
+
+def ssh_execute(host, command):
+    """Execute a command over ssh and print the output"""
+    if not ping(host):
+        print(f"{host} not reachable, skipping")
+        return
+    child = open_subprocess(f"ssh {host} {command}")
+    output = child.communicate()
+    print(output[0])
+
+def main():
+    ssh_execute("rd8n-pi", "ping -c 4 10.0.0.10")
+
+if __name__ == "__main__":
+    main()
